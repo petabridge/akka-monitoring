a50bea95bfa8b51c046254a702d5bdc444dc2ffb
Aaron Stannard <aaron@markedup.com>
2014-07-07 13:04:27 -0700

    renamed our project

---
+++ src/Akka.Counters/Impl/CounterNames.cs
@@ -0,0 +1,64 @@
+﻿using Akka.Actor;
+
+namespace Akka.Counters.CounterImpl
+{
+    /// <summary>
+    /// A static list of names and naming schemes for built-in counters
+    /// </summary>
+    public static class CounterNames
+    {
+
+        #region Actor lifecycle counters
+
+        public const string ActorLifeCycleCategory = "Akka.NET Actor Lifecycle";
+        public const string TotalActorRestarts = "Actor Restarts";
+        public const string ActorRestartsPerSecond = "Actor Restarts / sec";
+        public const string TotalActors = "Total Actors";
+        public const string ActorsCreatedPerSecond = "Actors Created / sec";
+        public const string ActorsStoppedPerSecond = "Actors Stopped / sec";
+
+        #endregion
+
+        #region Actor message counters
+
+        public const string ActorMessagesCategory = "Akka.NET Actor Messages";
+        public const string TotalMessages = "Total Messages";
+        public const string MessagesPerSecond = "Messages / sec";
+        public const string TotalDeadletters = "Total Deadletters";
+        public const string DeadlettersPerSecond = "Deadletters / sec";
+        public const string TotalUnhandledMessages = "Total Unhandled Messages";
+        public const string UnhandledMessagesPerSecond = "Unhandled Messages / sec";
+
+        #endregion
+
+        #region Actor logging counters
+
+        public const string ActorLogsCategory = "Akka.NET Log Messages";
+        public const string TotalDebugMessages = "Total Debug Messages";
+        public const string DebugMessagesPerSecond = "Debug Messages / sec";
+
+        public const string TotalInfoMessages = "Total Info Messages";
+        public const string InfoMessagesPerSecond = "Info Messages / sec";
+
+        public const string TotalWarningMessages = "Total Warning Messages";
+        public const string WarningMessagesPerSecond = "Warning Messages / sec";
+
+        public const string TotalErrorMessages = "Total Error Messages";
+        public const string ErrorMessagesPerSecond = "Error Messages / sec";
+
+        #endregion
+
+        /// <summary>
+        /// Gets the name of a counter collect for a specific group of actors
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static string ActorSpecificCategory(IActorContext context)
+        {
+            return
+                string.Format(string.Format("Akka.NET ({0} / {1}) Actor Metrics", context.System.Name,
+                    context.Props.GetType()));
+        }
+
+    }
+}
---
+++ src/Akka.Counters/Impl/IActorPerformanceClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Akka.Counters
+{
+    /// <summary>
+    /// Interface used to describe all of the reporting tools for plugging metrics to monitoring services
+    /// 
+    /// Akka-Monitoring can technically report to multiple reporting services at once, if necessary.
+    /// </summary>
+    public interface IActorPerformanceClient
+    {
+        /// <summary>
+        /// Updates a counter by an arbitrary amount
+        /// </summary>
+        /// <param name="metricName">The name of the metric</param>
+        /// <param name="delta">The amount to update the counter by, usually 1 or -1 but it can be arbitrary</param>
+        /// <param name="sampleRate">
+        /// The sample rate used for pulling readings off of the counter.
+        /// 
+        /// A sample rate of 0.1, for instance, would only sample 1/10 items from the counter onto the graph.
+        /// 
+        /// Implementation of the sample rate depends entirely on the backend being used for monitoring.
+        /// </param>
+        void UpdateCounter(string metricName, int delta, double sampleRate);
+
+        /// <summary>
+        /// Updates a time-based counter with the duration it took to complete an operation
+        /// </summary>
+        /// <param name="metricName">The name of the metric</param>
+        /// <param name="time">The amount of time elasped in recording this timer</param>
+        void UpdateTimer(string metricName, long time);
+
+        /// <summary>
+        /// Updates a Gauge, which is an arbitrary value that can be recorded. Might not be supported on your monitoring back-end. 
+        /// </summary>
+        /// <param name="metricName">The name of the metric</param>
+        /// <param name="value">The amount to pass to the gauge</param>
+        void UpdateGauge(string metricName, int value);
+    }
+}
